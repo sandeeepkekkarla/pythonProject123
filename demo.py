a0a1930b8601b14c48f17fc887f8190aeacf3132
sandeepkekkarla <sandysmile2424@gmailcom>
2022-08-19 14:51:20 +0530

    sandy update file demo

--- demo.py
+++ demo.py
@@ -3,4 +3,15 @@ def myfunc():
     print("welcome")
 print("demo module name is:", __name__)
 print(dir(p))
+count = 0
+n = int(input("enter a number"))
+for i in range(1, n + 1):
+    if n % i == 0:
+        count += 1
+if count == 2:
+    print(n, "is a prime num")
+else:
+    print(n, "is a not prime num")
+
+
 
--- test.py
+++ test.py
@@ -8,4 +8,15 @@ if __name__=="__main__":
     print(demo.p)
     demo.myfunc()
     print("test module name is:",__name__)
+count = 0
+n = int(input("enter a number"))
+for i in range(1, n + 1):
+    if n % i == 0:
+        count += 1
+if count == 2:
+    print(n, "is a prime num")
+else:
+    print(n, "is a not prime num")
+
+
 
